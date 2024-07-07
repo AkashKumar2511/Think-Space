ad1c7b3cc73e4d4ae06e220cd78e6f488c7435ea
Akash <akashkumar.sripatnala@gmail.com>
2024-07-07 23:26:29 +0530

    Add main body and footer of the landing page

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Think Space",
+  description: "The connected workspace for productivity boost",
+  icons: {
+    icon: {
+      url: "/favicon.ico",
+      href : "/favicon.ico"
+    },
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
